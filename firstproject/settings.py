186b72561b7d23e8ee3c75af762ed32f0c787900
syp <p1645@naver.com>
2019-07-31 00:24:55 +0900

    secret key

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'yz34hlf!!#%58%8m0a5zj)&hdl2z9)o+c2j*9a0hdw^t2&f53&'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'yz34hlf!!#%58%8m0a5zj)&hdl2z9)o+c2j*9a0hdw^t2&f53&')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
